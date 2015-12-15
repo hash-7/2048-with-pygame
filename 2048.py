3efa45f954a82ec7605e53ebbaa30850085b34fa
Isabel Lee <ijl0322@gmail.com>
2015-12-15 16:31:15 +0800

    refactoring

--- 2048.py
+++ 2048.py
@@ -6,25 +6,16 @@ pygame.init()
 window = pygame.display.set_mode((450, 550), 0, 32)
 pygame.display.set_caption("2048")
 
-
-
+#Setting color & font
 WHITE = (255,255,255)
 GREEN = (77,204,0)
 RED = (255,0,0)
 BLUE = (0,0,255)
-TEAL = (0,255,255)
-
-board = init_board()
-
 myfont = pygame.font.SysFont(None, 50)
 
-blocks = []
-for i in range(4):
-    for j in range(4):
-        blocks.append([pygame.Rect((i*100) + 30 , (j*100) + 135, 90, 90), WHITE])
 
-def buildText(i,j):
-    
+def buildText(board,i,j):
+        
     if board[j][i] == "0":        
         text = myfont.render(" ", True, BLUE)
     else:
@@ -34,23 +25,30 @@ def buildText(i,j):
     textRect.centery = j*100 + 180
     return text, textRect
 
-def showText():
 
+def showText(board):    
+    """Display numbers on screen"""
+    
     for i in range(4):
         for j in range(4):
-            window.blit(buildText(i,j)[0], buildText(i,j)[1])
+            window.blit(buildText(board,i,j)[0], buildText(board,i,j)[1])
+
+def quitWindow(event):
+    if event.type == QUIT:
+        pygame.quit()
+        sys.exit()
+    
 
 def gameOver():
-    #window.fill(WHITE)
+
     label = myfont.render("GAME OVER", True, RED)
     labelRect = label.get_rect()
     labelRect.centerx = window.get_rect().centerx
     labelRect.centery = window.get_rect().centery
-    window.blit(label, labelRect)
+    window.blit(label, labelRect) 
     event = pygame.event.wait()
-    if event.type == QUIT:
-        pygame.quit()
-        sys.exit()
+    quitWindow(event)
+    
 
 def win():
     window.fill(WHITE)
@@ -60,48 +58,49 @@ def win():
     labelRect.centery = window.get_rect().centery
     window.blit(label, labelRect)
     event = pygame.event.wait()
-    if event.type == QUIT:
-        pygame.quit()
-        sys.exit()
+    quitWindow(event)
     
-    
-    
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-
-        if event.type == KEYDOWN:
-            if event.key == K_UP:                
-                board = main(board, "u")
-            if event.key == K_DOWN:                
-                board = main(board, "d")
-            if event.key == K_LEFT:                
-                board = main(board, "l")
-            if event.key == K_RIGHT:                
-                board = main(board, "r")
-
-
-
-    window.fill(WHITE)
-    header = myfont.render("2048", True, BLUE)
-    window.blit(header, (30, 50))
-    pygame.draw.rect(window, GREEN, pygame.Rect(25, 130, 400, 400))
-
         
-    for block in blocks:
-        pygame.draw.rect(window, block[1], block[0])
-
+def gameLoop():
+    
+    #Initialize a game board with 16 blocks to store number
+    board = init_board()
+    blocks = []
+    for i in range(4):
+        for j in range(4):
+            blocks.append([pygame.Rect((i*100) + 30 , (j*100) + 135, 90, 90), WHITE])
         
-    showText()
+    while True:
+        for event in pygame.event.get():
+            
+            quitWindow(event)
+            if event.type == KEYDOWN:
+                if event.key == K_UP:                
+                    board = main(board, "u")
+                if event.key == K_DOWN:                
+                    board = main(board, "d")
+                if event.key == K_LEFT:                
+                    board = main(board, "l")
+                if event.key == K_RIGHT:                
+                    board = main(board, "r")
 
-    if checkLose(board):
-        gameOver()
-    elif checkWin(board):
-        win()
-    
-    pygame.display.update()
 
-    time.sleep(0.02)
+        window.fill(WHITE)
+        header = myfont.render("2048", True, BLUE)
+        window.blit(header, (30, 50))
+        pygame.draw.rect(window, GREEN, pygame.Rect(20, 125, 410, 410))
+        
+        for block in blocks:
+            pygame.draw.rect(window, block[1], block[0])        
+        showText(board)
+
+        if checkLose(board):
+            gameOver()
+        elif checkWin(board):
+            win()
+        
+        pygame.display.update()
+
+        time.sleep(0.02)
     
+gameLoop()
--- moves.py
+++ moves.py
@@ -11,6 +11,7 @@ def init_board():
     addNewNum(board, 2)  
     
     #debugging code
+
     """
     board[0][0] = "2"
     board[0][1] = "4"
@@ -23,20 +24,21 @@ def init_board():
     board[2][0] = "4"   
     board[2][1] = "2"
     board[2][2] = "32"
-    board[2][3] = "1024"
+    board[2][3] = "256"
     board[3][0] = "8"
     board[3][1] = "4"  
     board[3][2] = "1024"
-    board[3][3] = "516"   
+    board[3][3] = "1024"   
     """
-        
+    
     return board    
                        
                                                 
 def addNewNum(board, n):
     
     """Generates a 2 or 4, and set the number at a random location on board \
-    that is originally a 0 """
+    that is originally a 0.
+    """
     
     for i in range(n):
         newNum = str(random.choice([2,4]))
@@ -49,8 +51,10 @@ def addNewNum(board, n):
         
     
 def checkWin(board):
+    
     """ Checks if the player has the number 2048 on the board. 
-    If so, return True and print a "you win" statement. Otherwise, return False."""
+    If so, return True. Otherwise, return False.
+    """
     
     win = False
     for line in board:
@@ -71,37 +75,45 @@ def add(board, i_list, j_list, i_direction, j_direction):
     move is a counter that's later used to determine whether a move can still be made by the player.
     """
 
+    move = 0
     for i in i_list:
         for j in j_list:
                 
         #check if 2 numbers are the same, if yes, add them together
             if board[i][j] == board[i + i_direction][j + j_direction]:
                 board[i+ i_direction][j + j_direction] = str(int(board[i][j])+int(board[i+ i_direction][j+j_direction]))
+                if board[i][j] != 0:
+                    move += 1
                 board[i][j] = "0"
+    return move
 
     
 def push(board, i_list, j_list, i_direction, j_direction):
     
-    """push a number to its adjacent slot if the slot is 0. 
+    """Push a number to its adjacent slot if the slot is 0. 
     i_list, j_list - lists, indicate how push interate through the list.
     i_direction, j_direction - an int, either 1, -1 or 0. Defines the direction of the push. 
     move is a counter that's later used to determine whether a move can still be made by the player.
     """
-    
+
+    move = 0
     for i in i_list:
         for j in j_list:
             if board[i + i_direction][j + j_direction] == "0":
                 board[i + i_direction][j + j_direction] = board[i][j]
+                if board[i][j] != 0:
+                    move += 1
                 board[i][j] = "0"
+    return move
 
 
 def pushDirection(board, UserInput):
-    """
-    Takes in a UserInput and calls add and push function with the proper i_list,\
+    """Takes in a UserInput and calls add and push function with the proper i_list,\
     j_list, i_direction and j_direction. 
     UserInput - a str
     """
-    
+
+    move = 0
     if UserInput == "u":
         i_list, j_list = range(1,4), range(4)
         i_direction, j_direction = -1, 0
@@ -116,13 +128,21 @@ def pushDirection(board, UserInput):
         i_direction, j_direction = 0, 1
        
     for i in range(4):
-        push(board, i_list, j_list, i_direction, j_direction)
-    add(board, i_list, j_list, i_direction, j_direction)
+        move += push(board, i_list, j_list, i_direction, j_direction)
+    move += add(board, i_list, j_list, i_direction, j_direction)
     for i in range(4):
-        push(board, i_list, j_list, i_direction, j_direction)
+        move += push(board, i_list, j_list, i_direction, j_direction)
     
+    return move
+
 
 def checkCell(board, i, j):
+    """Checks the cell above/ below/ to the left/ to the right
+    of board[i][j] and see if one or more equals to board[i][j]
+    if so, return True.
+    else, return False.
+    """
+    
     move_i = []
     move_j = []
     board_size = len(board)
@@ -145,6 +165,9 @@ def checkCell(board, i, j):
 
 
 def canMove(board):
+    """Checks if the player can still can still make a move.
+    If so, return True. Else, return False.
+    """
 
     board_size = len(board)
     for i in range(board_size):
@@ -157,8 +180,11 @@ def canMove(board):
 
 def checkLose(board):
     
-    """Takes in a list (board), return True the player does not lose (if there is still 0 in the list or \
-    if any move can still be made) otherwise False"""
+    """Takes in a list (board), return True the player does not lose
+    (if there is still 0 in the list or \ if any move can still be made)
+    otherwise False
+    """
+    
     nozero = False
     
     for elt in board:
